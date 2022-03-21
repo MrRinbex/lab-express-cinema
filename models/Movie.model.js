db53fb38e85e146c185517c5a30f0728edf4b332
Léo Cheffi <contact@leocheffi.com>
2022-03-21 17:49:09 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose')
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const movieSchema = new Schema({
+  title: {
+    type: String,
+    unique : true
+},
+    director : String,
+    stars : [String],
+    image : String,
+    description : String,
+    showtimes : [String]
+
+})
+
+const Movie = model('Movie', movieSchema)
+
+module.exports = Movie
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const Movie = require('../models/Movie.model')
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
-router.get('/movies', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
     const movies = await Movie.find()
     res.render('movies', { movies })
   })
-router.get('/movie/:id', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     
     const movie = await Movie.findById(req.params.id)
     res.render('movie', { movie })
